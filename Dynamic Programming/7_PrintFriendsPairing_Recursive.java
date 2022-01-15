76f271227955da1dd8c0a457ae5dc7b83135a41d
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-01-15 20:16:26 +0530

    Create 7_PrintFriendsPairing_Recursive.java

---
+++ Dynamic Programming/7_PrintFriendsPairing_Recursive.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println(printFriends("ABCDEF", ""));
+    }
+    
+    public static long printFriends(String str, String asf){
+        if(str.length() == 0){
+            System.out.println(asf);
+            return 1;
+        }
+        long count = 0;
+        char ch = str.charAt(0);
+        count += printFriends(str.substring(1), asf + ch + " ");
+        for(int i = 1; i < str.length(); i++){
+            String ros = str.substring(1, i) + str.substring(i + 1);
+            count += printFriends(ros, asf + ch + str.charAt(i) + " ");
+        }
+        return count;
+    }
+}
