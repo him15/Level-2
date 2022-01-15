a8bb3d3faf82c2a4b7b22986ef2169e5dd2492ae
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-01-15 09:14:43 +0530

    Create 4_DiceThrow.java

---
+++ Dynamic Programming/4_DiceThrow.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    // int m = sc.nextInt();
+    int[] dp = new int[n + 1];
+
+    // System.out.println(diceThrowRecursive(n));           // recursive solution
+    // System.out.println(diceThrow_Memo(n, dp));  // memo
+    System.out.println(diceThrow_DP(n, dp));    //dp
+    print1D(dp);
+    // print2D(dp);
+  }
+
+  public static int diceThrowRecursive(int n) {
+    if (n == 0) {
+      return 1;
+    }
+    int count = 0;
+    for (int jump = 1; jump <= 6; jump++) {
+      if (n - jump >= 0) {
+        count += diceThrowRecursive(n - jump);
+      }
+    }
+    return count;
+  }
+
+  public static int diceThrow_Memo(int n, int[] dp) {
+    if (n == 0) {
+      return dp[n] = 1;
+    }
+    if (dp[n] != 0) {
+      return dp[n];
+    }
+    int count = 0;
+    for (int jump = 1; jump <= 6; jump++) {
+      if (n - jump >= 0) {
+        count += diceThrow_Memo(n - jump, dp);
+      }
+    }
+    return dp[n] = count;
+  }
+
+  public static int diceThrow_DP(int N, int[] dp) {
+    for (int n = 0; n <= N; n++) {
+      if (n == 0) {
+        dp[n] = 1;
+        continue;
+      }
+      int count = 0;
+      for (int jump = 1; jump <= 6; jump++) {
+        if (n - jump >= 0) {
+          count += dp[n - jump]; //diceThrow_Memo(n - jump, dp);
+        }
+      }
+      dp[n] = count;
+    }
+
+    return dp[N];
+  }
+
+  public static void print1D(int[] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+    System.out.println();
+  }
+
+  public static void print2D(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
