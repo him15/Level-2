7ab98223b3a890bb207f2d080fb100cf7ec17571
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-01-17 21:04:18 +0530

    Create 16_LongestPalindromicSubsequence.java

---
+++ Dynamic Programming/16_LongestPalindromicSubsequence.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int lcsDp(String s1, String s2, int N, int M, int[][] dp) {
+        for(int n = 0; n <= N; n++){
+            for(int m = 0; m <= M; m++){
+                if(n == 0 || m == 0){
+                    dp[n][m] = 0;
+                    continue;
+                }
+                int count = 0;
+                if(s1.charAt(n - 1) == s2.charAt(m - 1)){
+                    count += 1 + dp[n - 1][m - 1]; //longestCommonSubsequence_Memo(s1, s2, n - 1, m - 1, dp);
+                }else{
+                    // count += Math.max(longestCommonSubsequence_Memo(s1, s2, n, m - 1, dp), longestCommonSubsequence_Memo(s1, s2, n - 1, m, dp));
+                    count += Math.max(dp[n][m - 1], dp[n - 1][m]);
+                }
+                dp[n][m] = count;
+            }
+        }
+        return dp[N][M];
+    }
+    
+    public String reverse(String s){
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        return sb.toString();
+    }
+        
+    public int longestPalindromeSubseq(String s1) {
+        int n = s1.length();
+        String s2 = reverse(s1);
+        int m = s2.length();
+        
+        int[][] dp = new int[n + 1][m + 1];
+        int lcs = lcsDp(s1, s2, n, m, dp);
+        return lcs;
+    }
+}
