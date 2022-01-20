dfe04b30e3cd7e114e5adb78058c7f7319aad1f5
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-01-20 18:41:40 +0530

    Update 26_Minimum_Number_Of_Deletion_Required_To_Be_Performed_To_Make_An_Array_Sorted.java

--- Dynamic Programming/26_Minimum_Number_Of_Deletion_Required_To_Be_Performed_To_Make_An_Array_Sorted.java
+++ Dynamic Programming/26_Minimum_Number_Of_Deletion_Required_To_Be_Performed_To_Make_An_Array_Sorted.java
@@ -1,4 +1,6 @@
-All code are same like LIS (Longest Increasing Subsequence) but the change is check (arr[j] < arr[j]) this is in LIS but here the array contains duplicate numbers so we are checking (arr[j] <= arr[i]) and it gives the Longest Increasing Subsequence and now in the end we are substracting that from arrays length to get the final deletion count
+All code are same like LIS (Longest Increasing Subsequence) but the change is check (arr[j] < arr[j]) this is in LIS but here the array contains duplicate numbers so,
+we are checking (arr[j] <= arr[i]) and it gives the Longest Increasing Subsequence and now in the end,
+we are substracting that from arrays length to get the final deletion count
 
 // extention question Minimum number of deletion to be performed to make an array sorted constrins are (-1e7 <= ele <= 1e7)
     public int Min_Deletion(int[] arr, int[] dp){
