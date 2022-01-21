7e6834830fa0c36841290b5992ed3391642d9e17
Vivek Shinde <85720426+vivekshinde45@users.noreply.github.com>
2022-01-21 09:47:31 +0530

    Create 29_Longest_Bitonic_Subsequence_DownHILL.java

---
+++ Dynamic Programming/29_Longest_Bitonic_Subsequence_DownHILL.java
@@ -0,0 +1,44 @@
+class solution{
+// LIS - Longest Increasing Subsequence 
+    public int[] LIS_LR(int[] arr){
+        int n = arr.length;
+        int[] dp = new int[n];
+        for(int i = 0; i < n; i++){
+            dp[i] = 1;
+            for(int j = i - 1; j >= 0; j--){
+                if(arr[j] > arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+    
+    // LDS - Longest Decreasing Subsequence
+    public int[] LIS_RL(int[] arr){
+        int n = arr.length;
+        int[] dp = new int[n];
+        for(int i = n - 1; i >= 0; i--){
+            dp[i] = 1;
+            for(int j = i + 1; j < n; j++){
+                if(arr[j] > arr[i]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return dp;
+    }
+    
+    
+    public int LongestBitonicSequence_DownHILL(int[] nums) {
+        int n = nums.length;
+        int[] LIS = LIS_LR(nums);
+        int[] LDS = LIS_RL(nums);
+        
+        int len = 0;
+        for(int i = 0; i < n; i++){
+            len = Math.max(len, LIS[i] + LDS[i] - 1);
+        }
+        return len;
+    }
+}
